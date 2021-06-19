cb2dbc34a1323ab91d7b4bdc31281c51369532db
kai5363 <57265307+kai5363@users.noreply.github.com>
2021-06-20 01:38:47 +0800

    version 2.0

--- all.js
+++ all.js
@@ -3,13 +3,11 @@ const btn_add = document.querySelector(".btn_add");
 const btn_all = document.querySelector(".btn_all");
 const btn_unfinished = document.querySelector(".btn_unfinished");
 const btn_finished = document.querySelector(".btn_finished");
-const todolist = document.querySelector(".todolist");
+const todoList = document.querySelector(".todoList");
 const filter = document.querySelector(".filter");
 const unfinished_num = document.querySelector(".unfinished_num");
 const input_list = document.querySelector(".input_list");
-const clear_btn = document.querySelector(".clear_btn");
-
-
+const clearDone_btn = document.querySelector(".clearDone_btn");
 
 
 function calc_unfinished_num() {
@@ -24,24 +22,25 @@ function calc_unfinished_num() {
 
 function render() {
     let str = "";
-    //判斷cursor在哪
     data.forEach(function (item, index) {
         let content;
         if (data[index].checked) {
             content = `<li>
-            <label data-num="${index}" ><input data-num="${index}" class="checkbox" type="checkbox" checked><span >✔</span>
-            <p>${item.todolist}</p>
+            <label data-num="${index}" ><input data-num="${index}" class="checkBox" type="checkbox" checked><span >✔</span>
+            <p>${item.todoList}</p>
             </label><input class="delete" type="button" value="X"></li>`;
         } else {
             content = `<li>
-            <label data-num="${index}"><input data-num="${index}" class="checkbox" type="checkbox"><span >✔</span>
-            <p>${item.todolist}</p>
+            <label data-num="${index}"><input data-num="${index}" class="checkBox" type="checkbox"><span >✔</span>
+            <p>${item.todoList}</p>
             </label><input class="delete" type="button" value="X"></li>`;
         }
         str += content
     })
-    todolist.innerHTML = str;
+    todoList.innerHTML = str;
     calc_unfinished_num()
+
+    //判斷cursor在哪
     let cursor = filter.querySelector(".cursor");
     refresh_filter(cursor);
 }
@@ -57,18 +56,16 @@ function calc_unfinished_num() {
 }
 
 function refresh_filter(tag) {
-    let check = todolist.querySelectorAll(".checkbox:checked");
-    let uncheck = todolist.querySelectorAll(".checkbox:not(:checked)");
-    let checkbox = todolist.querySelectorAll(".checkbox");
+    let checkBox = todoList.querySelectorAll(".checkBox");
 
     if (tag.value === "已完成") {
         btn_unfinished.classList.remove("cursor");
         btn_all.classList.remove("cursor");
         for (let i = 0; i < data.length; i++) {
             if (data[i].checked) {
-                checkbox[i].parentNode.parentNode.style.display = 'flex';
+                checkBox[i].parentNode.parentNode.style.display = 'flex';
             } else {
-                checkbox[i].parentNode.parentNode.style.display = 'none';
+                checkBox[i].parentNode.parentNode.style.display = 'none';
             }
         }
     } else if (tag.value === "待完成") {
@@ -76,9 +73,9 @@ function refresh_filter(tag) {
         btn_finished.classList.remove("cursor");
         for (let i = 0; i < data.length; i++) {
             if (data[i].checked) {
-                checkbox[i].parentNode.parentNode.style.display = 'none';
+                checkBox[i].parentNode.parentNode.style.display = 'none';
             } else {
-                checkbox[i].parentNode.parentNode.style.display = 'flex';
+                checkBox[i].parentNode.parentNode.style.display = 'flex';
             }
         }
 
@@ -86,7 +83,7 @@ function refresh_filter(tag) {
         btn_finished.classList.remove("cursor");
         btn_unfinished.classList.remove("cursor");
         for (let i = 0; i < data.length; i++) {
-            checkbox[i].parentNode.parentNode.style.display = 'flex';
+            checkBox[i].parentNode.parentNode.style.display = 'flex';
         }
     }
 
@@ -95,14 +92,14 @@ function refresh_filter(tag) {
 render()
 
 // check and uncheck 
-todolist.addEventListener("click", function (e) {
+todoList.addEventListener("click", function (e) {
     let num = e.target.dataset.num;
-    let checkbox = todolist.querySelectorAll(".checkbox");
+    let checkBox = todoList.querySelectorAll(".checkBox");
     if (e.target.nodeName !== "INPUT" || e.target.className === "delete") {
         return
     }
-    // console.log(checkbox[num].checked);
-    if (checkbox[num].checked) {
+    // console.log(checkBox[num].checked);
+    if (checkBox[num].checked) {
         data[num].checked = true;
     } else {
         data[num].checked = false;
@@ -112,7 +109,7 @@ todolist.addEventListener("click", function (e) {
     refresh_filter(cursor);
 })
 
-// add new todolist
+// add new todoList
 btn_add.addEventListener("click", function (e) {
     let content = input_list.value.trim();
     if (content === "") {
@@ -120,7 +117,7 @@ btn_add.addEventListener("click", function (e) {
     }
     data.push({
         checked: false,
-        todolist: content
+        todoList: content
     });
     render()
     input_list.value = "";
@@ -135,21 +132,22 @@ input_list.addEventListener("keyup", function (e) {
 
 
 filter.addEventListener("click", function (e) {
-    let check = todolist.querySelectorAll(".checkbox:checked");
-    let uncheck = todolist.querySelectorAll(".checkbox:not(:checked)");
-    let checkbox = todolist.querySelectorAll(".checkbox");
     e.target.classList.add("cursor");
     refresh_filter(e.target)
 
 })
 
-clear_btn.addEventListener("click", function (e) {
-    data = [];
+clearDone_btn.addEventListener("click", function () {
+    for (let i = data.length - 1; i > -1; i--) {
+        if (data[i].checked) {
+            data.splice(i, 1);
+        }
+    }
     render()
 })
 
 
-todolist.addEventListener("click", function (e) {
+todoList.addEventListener("click", function (e) {
     if (e.target.className !== "delete") {
         return
     }
